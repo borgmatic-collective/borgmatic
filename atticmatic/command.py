7097ed67a6b9a3035112dade86ce5cbcd4a778e6
Dan Helfman <dan@energysavvy.com>
2015-07-18 18:35:29 -0700

    New "borgmatic" command to support Borg backup software, a fork of Attic.

--- atticmatic/command.py
+++ atticmatic/command.py
@@ -1,31 +1,34 @@
 from __future__ import print_function
 from argparse import ArgumentParser
+from importlib import import_module
+import os
 from subprocess import CalledProcessError
 import sys
 
-from atticmatic.attic import check_archives, create_archive, prune_archives
 from atticmatic.config import parse_configuration
 
 
-DEFAULT_CONFIG_FILENAME = '/etc/atticmatic/config'
-DEFAULT_EXCLUDES_FILENAME = '/etc/atticmatic/excludes'
+DEFAULT_CONFIG_FILENAME_PATTERN = '/etc/{}/config'
+DEFAULT_EXCLUDES_FILENAME_PATTERN = '/etc/{}/excludes'
 
 
-def parse_arguments(*arguments):
+def parse_arguments(command_name, *arguments):
     '''
-    Parse the given command-line arguments and return them as an ArgumentParser instance.
+    Given the name of the command with which this script was invoked and command-line arguments,
+    parse the arguments and return them as an ArgumentParser instance. Use the command name to
+    determine the default configuration and excludes paths.
     '''
     parser = ArgumentParser()
     parser.add_argument(
         '-c', '--config',
         dest='config_filename',
-        default=DEFAULT_CONFIG_FILENAME,
+        default=DEFAULT_CONFIG_FILENAME_PATTERN.format(command_name),
         help='Configuration filename',
     )
     parser.add_argument(
         '--excludes',
         dest='excludes_filename',
-        default=DEFAULT_EXCLUDES_FILENAME,
+        default=DEFAULT_EXCLUDES_FILENAME_PATTERN.format(command_name),
         help='Excludes filename',
     )
     parser.add_argument(
@@ -37,15 +40,30 @@ def parse_arguments(*arguments):
     return parser.parse_args(arguments)
 
 
+def load_backend(command_name):
+    '''
+    Given the name of the command with which this script was invoked, return the corresponding
+    backend module responsible for actually dealing with backups.
+    '''
+    backend_name = {
+        'atticmatic': 'attic',
+        'borgmatic': 'borg',
+    }.get(command_name, 'attic')
+
+    return import_module('atticmatic.backends.{}'.format(backend_name))
+
+
 def main():
     try:
-        args = parse_arguments(*sys.argv[1:])
+        command_name = os.path.basename(sys.argv[0])
+        args = parse_arguments(command_name, *sys.argv[1:])
         config = parse_configuration(args.config_filename)
         repository = config.location['repository']
+        backend = load_backend(command_name)
 
-        create_archive(args.excludes_filename, args.verbosity, **config.location)
-        prune_archives(args.verbosity, repository, config.retention)
-        check_archives(args.verbosity, repository, config.consistency)
+        backend.create_archive(args.excludes_filename, args.verbosity, **config.location)
+        backend.prune_archives(args.verbosity, repository, config.retention)
+        backend.check_archives(args.verbosity, repository, config.consistency)
     except (ValueError, IOError, CalledProcessError) as error:
         print(error, file=sys.stderr)
         sys.exit(1)
--- atticmatic/tests/integration/test_command.py
+++ atticmatic/tests/integration/test_command.py
@@ -5,16 +5,19 @@ from nose.tools import assert_raises
 from atticmatic import command as module
 
 
-def test_parse_arguments_with_no_arguments_uses_defaults():
-    parser = module.parse_arguments()
+COMMAND_NAME = 'foomatic'
 
-    assert parser.config_filename == module.DEFAULT_CONFIG_FILENAME
-    assert parser.excludes_filename == module.DEFAULT_EXCLUDES_FILENAME
+
+def test_parse_arguments_with_no_arguments_uses_defaults():
+    parser = module.parse_arguments(COMMAND_NAME)
+
+    assert parser.config_filename == module.DEFAULT_CONFIG_FILENAME_PATTERN.format(COMMAND_NAME)
+    assert parser.excludes_filename == module.DEFAULT_EXCLUDES_FILENAME_PATTERN.format(COMMAND_NAME)
     assert parser.verbosity == None
 
 
 def test_parse_arguments_with_filename_arguments_overrides_defaults():
-    parser = module.parse_arguments('--config', 'myconfig', '--excludes', 'myexcludes')
+    parser = module.parse_arguments(COMMAND_NAME, '--config', 'myconfig', '--excludes', 'myexcludes')
 
     assert parser.config_filename == 'myconfig'
     assert parser.excludes_filename == 'myexcludes'
@@ -22,10 +25,10 @@ def test_parse_arguments_with_filename_arguments_overrides_defaults():
 
 
 def test_parse_arguments_with_verbosity_flag_overrides_default():
-    parser = module.parse_arguments('--verbosity', '1')
+    parser = module.parse_arguments(COMMAND_NAME, '--verbosity', '1')
 
-    assert parser.config_filename == module.DEFAULT_CONFIG_FILENAME
-    assert parser.excludes_filename == module.DEFAULT_EXCLUDES_FILENAME
+    assert parser.config_filename == module.DEFAULT_CONFIG_FILENAME_PATTERN.format(COMMAND_NAME)
+    assert parser.excludes_filename == module.DEFAULT_EXCLUDES_FILENAME_PATTERN.format(COMMAND_NAME)
     assert parser.verbosity == 1
 
 
@@ -35,6 +38,6 @@ def test_parse_arguments_with_invalid_arguments_exits():
 
     try:
         with assert_raises(SystemExit):
-            module.parse_arguments('--posix-me-harder')
+            module.parse_arguments(COMMAND_NAME, '--posix-me-harder')
     finally:
         sys.stderr = original_stderr
