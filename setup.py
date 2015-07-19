1f1c8fdabacdd2a8b896fa1219be5a6de86a0820
Dan Helfman <witten@torsion.org>
2015-07-18 23:48:58 -0700

    Bumping version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='atticmatic',
-    version='0.1.0',
+    version='0.1.1',
     description='A wrapper script for Attic/Borg backup software that creates and prunes backups',
     author='Dan Helfman',
     author_email='witten@torsion.org',
