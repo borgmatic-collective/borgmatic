c554d1d36d67711ac4bbaa28d525901438acfec8
Dan Helfman <witten@torsion.org>
2015-08-09 10:59:27 -0700

    Changes to support release on PyPI. Now pip installable by name!

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import setup, find_packages
 
+
+VERSION = '0.1.5'
+
+
 setup(
     name='atticmatic',
-    version='0.1.4',
+    version=VERSION,
     description='A wrapper script for Attic/Borg backup software that creates and prunes backups',
     author='Dan Helfman',
     author_email='witten@torsion.org',
+    url='https://torsion.org/atticmatic',
+    download_url='https://torsion.org/hg/atticmatic/archive/%s.tar.gz' % VERSION,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Topic :: Security :: Cryptography',
+        'Topic :: System :: Archiving :: Backup',
+    ),
     packages=find_packages(),
     entry_points={
         'console_scripts': [
