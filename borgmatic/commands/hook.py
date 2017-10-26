a09c9f248e7c25200a9d7102ead87a619f5fde83
Dan Helfman <witten@torsion.org>
2017-10-25 22:32:06 -0700

    Adding logging to hook execution!

--- borgmatic/commands/hook.py
+++ borgmatic/commands/hook.py
@@ -1,7 +1,20 @@
+import logging
 import subprocess
 
 
-def execute_hook(commands):
-    if commands:
-        for cmd in commands:
-            subprocess.check_call(cmd, shell=True)
+logger = logging.getLogger(__name__)
+
+
+def execute_hook(commands, config_filename, description):
+    if not commands:
+        logger.debug('{}: No commands to run for {} hook'.format(config_filename, description))
+        return
+
+    if len(commands) == 1:
+        logger.info('{}: Running command for {} hook'.format(config_filename, description))
+    else:
+        logger.info('{}: Running {} commands for {} hook'.format(config_filename, len(commands), description))
+
+    for command in commands:
+        logger.debug('{}: Hook command: {}'.format(config_filename, command))
+        subprocess.check_call(command, shell=True)
--- borgmatic/tests/unit/borg/test_hook.py
+++ borgmatic/tests/unit/borg/test_hook.py
@@ -7,4 +7,16 @@ def test_execute_hook_invokes_each_command():
     subprocess = flexmock(module.subprocess)
     subprocess.should_receive('check_call').with_args(':', shell=True).once()
 
-    module.execute_hook([':'])
+    module.execute_hook([':'], 'config.yaml', 'pre-backup')
+
+
+def test_execute_hook_with_multiple_commands_invokes_each_command():
+    subprocess = flexmock(module.subprocess)
+    subprocess.should_receive('check_call').with_args(':', shell=True).once()
+    subprocess.should_receive('check_call').with_args('true', shell=True).once()
+
+    module.execute_hook([':', 'true'], 'config.yaml', 'pre-backup')
+
+
+def test_execute_hook_with_empty_commands_does_not_raise():
+    module.execute_hook([], 'config.yaml', 'post-backup')
