cc94e5f52f330fba5c4b2a6d06eaeb37c0ea392d
Dan Helfman <witten@torsion.org>
2017-10-25 22:36:23 -0700

    Add tests for verbosity mapping.

---
+++ borgmatic/tests/unit/test_verbosity.py
@@ -0,0 +1,11 @@
+import logging
+
+from borgmatic import verbosity as module
+
+
+def test_verbosity_to_log_level_maps_known_verbosity_to_log_level():
+    assert module.verbosity_to_log_level(module.VERBOSITY_SOME) == logging.INFO
+
+
+def test_verbosity_to_log_level_maps_unknown_verbosity_to_error_level():
+    assert module.verbosity_to_log_level('my pants') == logging.ERROR
