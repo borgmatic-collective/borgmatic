059322b7f8da536899408ae887c939d4056bef0c
Dan Helfman <witten@torsion.org>
2017-10-25 21:47:33 -0700

    Improve clarity of logging spew at high verbosity levels.

--- borgmatic/verbosity.py
+++ borgmatic/verbosity.py
@@ -1,2 +1,17 @@
+import logging
+
+
 VERBOSITY_SOME = 1
 VERBOSITY_LOTS = 2
+
+
+def verbosity_to_log_level(verbosity):
+    '''
+    Given a borgmatic verbosity value, return the corresponding Python log level.
+    '''
+    return {
+        VERBOSITY_SOME: logging.INFO,
+        VERBOSITY_LOTS: logging.DEBUG,
+    }.get(verbosity, logging.ERROR)
+
+
