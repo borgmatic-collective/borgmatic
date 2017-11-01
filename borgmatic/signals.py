f017ed648fa9c68c2f5c4415a60d954be0ab6ebf
Dan <dan@energysavvy.com>
2017-10-31 22:01:18 -0700

    Disabling code coverage on this one-line functions.

--- borgmatic/signals.py
+++ borgmatic/signals.py
@@ -2,14 +2,14 @@ import os
 import signal
 
 
-def _handle_signal(signal_number, frame):
+def _handle_signal(signal_number, frame):  # pragma: no cover
     '''
     Send the signal to all processes in borgmatic's process group, which includes child process.
     '''
     os.killpg(os.getpgrp(), signal_number)
 
 
-def configure_signals():
+def configure_signals():  # pragma: no cover
     '''
     Configure borgmatic's signal handlers to pass relevant signals through to any child processes
     like Borg.
