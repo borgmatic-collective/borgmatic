205adaba470eaa6bf9f2fa2eb6a6c6fa62fcfcf5
Nick Whyte <nick@nickwhyte.com>
2018-03-04 17:12:29 +1100

    .

--- borgmatic/verbosity.py
+++ borgmatic/verbosity.py
@@ -1,7 +1,6 @@
 import logging
 
 
-VERBOSITY_WARNING = 0
 VERBOSITY_SOME = 1
 VERBOSITY_LOTS = 2
 
@@ -11,7 +10,6 @@ def verbosity_to_log_level(verbosity):
     Given a borgmatic verbosity value, return the corresponding Python log level.
     '''
     return {
-        VERBOSITY_WARNING: logging.WARNING,
         VERBOSITY_SOME: logging.INFO,
         VERBOSITY_LOTS: logging.DEBUG,
     }.get(verbosity, logging.WARNING)
