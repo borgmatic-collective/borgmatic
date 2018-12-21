c06f9f4cbde3074ab390903f9058141ce095c1f0
Anton Anikin <anton@anikin.xyz>
2018-12-21 12:44:57 +0800

    Add --stats option

--- borgmatic/commands/borgmatic.py
+++ borgmatic/commands/borgmatic.py
@@ -108,6 +108,14 @@ def parse_arguments(*arguments):
         help='Display progress with --create option for each file as it is backed up',
     )
     parser.add_argument(
+        '-s',
+        '--stats',
+        dest='stats',
+        default=False,
+        action='store_true',
+        help='Print statistics for the created archive',
+    )
+    parser.add_argument(
         '--json',
         dest='json',
         default=False,
@@ -151,6 +159,9 @@ def parse_arguments(*arguments):
 
     if args.progress and not args.create:
         raise ValueError('The --progress option can only be used with the --create option')
+
+    if args.stats and not args.create:
+        raise ValueError('The --stats option can only be used with the --create option')
 
     if args.json and not (args.create or args.list or args.info):
         raise ValueError(
@@ -272,6 +283,7 @@ def _run_commands_on_repository(
             local_path=local_path,
             remote_path=remote_path,
             progress=args.progress,
+            stats=args.stats
         )
     if args.check and checks.repository_enabled_for_checks(repository, consistency):
         logger.info('{}: Running consistency checks'.format(repository))
