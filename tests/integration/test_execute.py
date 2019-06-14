1ba56d5262bdda416d956735d603e6725ac41aa3
Dan Helfman <witten@torsion.org>
2019-06-13 21:34:04 -0700

    Fix tests in Python 3.6.

--- tests/integration/test_execute.py
+++ tests/integration/test_execute.py
@@ -11,15 +11,15 @@ def test_execute_and_log_output_logs_each_line_separately():
     flexmock(module.logger).should_receive('log').with_args(logging.INFO, 'hi').once()
     flexmock(module.logger).should_receive('log').with_args(logging.INFO, 'there').once()
 
-    module.execute_and_log_output(['echo', 'hi\nthere'], output_log_level=logging.INFO, shell=False)
+    module.execute_and_log_output(['echo', 'hi'], output_log_level=logging.INFO, shell=False)
+    module.execute_and_log_output(['echo', 'there'], output_log_level=logging.INFO, shell=False)
 
 
 def test_execute_and_log_output_logs_borg_error_as_error():
     flexmock(module.logger).should_receive('error').with_args('borg: error: oopsie').once()
-    flexmock(module.logger).should_receive('log').with_args(logging.INFO, 'daisie').once()
 
     module.execute_and_log_output(
-        ['echo', 'borg: error: oopsie\ndaisie\n'], output_log_level=logging.INFO, shell=False
+        ['echo', 'borg: error: oopsie'], output_log_level=logging.INFO, shell=False
     )
 
 
