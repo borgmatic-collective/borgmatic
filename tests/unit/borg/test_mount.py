528dc6fe878a419e73550b2b40a15eb437f19956
Dan Helfman <witten@torsion.org>
2026-01-09 12:01:21 -0800

    For the "mount" action, fix use of "--log-json" when "--foreground" is set (#485).

--- tests/unit/borg/test_mount.py
+++ tests/unit/borg/test_mount.py
@@ -273,7 +273,7 @@ def test_mount_archive_calls_borg_with_foreground_parameter():
     flexmock(module.environment).should_receive('make_environment')
     flexmock(module.borgmatic.config.paths).should_receive('get_working_directory').and_return(None)
     flexmock(module).should_receive('execute_command').with_args(
-        ('borg', 'mount', '--log-json', '--foreground', 'repo::archive', '/mnt'),
+        ('borg', 'mount', '--foreground', 'repo::archive', '/mnt'),
         output_file=module.DO_NOT_CAPTURE,
         environment=None,
         working_directory=None,
