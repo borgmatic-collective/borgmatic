43d8c25234c0766e013b7db08f83513bd3a1b512
Dan Helfman <witten@torsion.org>
2026-03-02 23:19:03 -0800

    Disable BenchNine font the in borgmatic docs tarball, because Debian policy doesn't allow fonts they don't compile themselves.

--- docs/_data/borgmatic.js
+++ docs/_data/borgmatic.js
@@ -1,6 +1,7 @@
 module.exports = function() {
     return {
         environment: process.env.NODE_ENV || "development",
-        port: process.env.PORT || 8080
+        port: process.env.PORT || 8080,
+        font: process.env.FONT || "custom"
     };
 };
